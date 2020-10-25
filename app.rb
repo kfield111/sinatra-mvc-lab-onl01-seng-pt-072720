0aebe82f3d1d3a02b7ad52b3145fcbc15beb5bfc
Kelin Field <kfield111@hotmail.com>
2020-10-25 18:51:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piglatinized_text = PigLatinizer.new(params[:user_phrase])
+    @text = PigLatinizer.new(params[:user_phrase])
     erb :results
   end
 
